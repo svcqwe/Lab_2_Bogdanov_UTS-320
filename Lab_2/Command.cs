6aae184706a1c2a6b0c569d7335a7c82aa19d948
svcqwe <bogdanovmax41@gmail.com>
2025-10-12 10:06:34 +0300

    То, что сделано на лабе

---
+++ Lab_2/Command.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_2
+{
+    internal class Command
+    {
+        public void delete(string path)
+        {
+            try
+            {
+                Directory.Delete(path, true);
+            }
+            catch
+            {
+                Console.WriteLine("Введен неавприльный путь!");
+            }
+        }
+        public void copy(string sourth_path, string goal_path)
+        {
+            Directory.Move(sourth_path, goal_path);
+        }
+        public void rename(string peth, string source_name, string new_name)
+        {
+            Console.WriteLine("sdf");
+        }
+
+    }
+}
---
+++ Lab_2/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.IO;
+
+namespace Lab_2
+{
+
+    class Programm
+    {
+        static void Main()
+        {
+            while (true)
+            {
+                Console.WriteLine("\nДоступные команды:");
+                Console.WriteLine("1. Просотр содержимого директории\n" +
+                                  "2. Удалить файл/папку\n" +
+                                  "3. Копировать файл/папку\n" +
+                                  "4. Переимновать файл/папку");
+                
+                int choose = int.Parse(Console.ReadLine());
+                
+                if (choose == 1)
+                {
+                    Console.WriteLine("Введи абсолютный путь до папки");
+                    string path = Console.ReadLine();
+                    try
+                    {
+                        file_manager File_manager = new file_manager();
+                        File_manager.Path(path);
+                    }
+                    catch
+                    {
+                        if (int.Parse(path) == 0)       //Заврешение работы программы
+                            break;
+                        else Console.WriteLine("Неверно введен путь до директории. Вводи абсолютный путь!");
+                    }
+                }
+                
+                if(choose == 2)
+                {
+                    Console.WriteLine("Введи абсолютный путь до директории/файла");
+                    string path = Console.ReadLine();
+                    try
+                    {
+                        Command command = new Command();
+                        command.delete(path);
+                    }
+                    catch
+                    {
+                        if (int.Parse(path) == 0)       //Заврешение работы программы
+                            break;
+                        else Console.WriteLine("Неверно введен путь до директории/папкм. Вводи абсолютный путь!");
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+
