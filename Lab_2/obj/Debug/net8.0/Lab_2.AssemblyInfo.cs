7f5b6333e6697b1ba967ca4c20363b74393f0913
svcqwe <bogdanovmax41@gmail.com>
2025-10-16 11:24:43 +0300

    2 версия (доделано дома)

--- Lab_2/obj/Debug/net8.0/Lab_2.AssemblyInfo.cs
+++ Lab_2/obj/Debug/net8.0/Lab_2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab_2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6aae184706a1c2a6b0c569d7335a7c82aa19d948")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab_2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab_2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
